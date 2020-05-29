e494e322c1143e1179edf950e628ade27f00555b
pmasa <masamuna.pedro@hotmail.com>
2020-05-29 11:06:17 +0200

    some clean up

--- laveMains.py
+++ laveMains.py
@@ -1,3 +1,53 @@
+import RPi.GPIO as GPIO
+import time
+
+import threading 
+
+GPIO.setmode(GPIO.BCM)
+
+relay_pin = 27
+PIR_SENSOR = 17
+
+GPIO.setup(PIR_SENSOR, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(relay_pin, GPIO.OUT)
+
+
+print ("Waiting for 2 seconds")
+time.sleep(2)
+
+
+print ("Start processing...")
+
+motion = False
+
+i = GPIO.input(PIR_SENSOR)
+time.sleep(5) # wait 5 seconds for PIR to reset.
+
+class Monthread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.encore = True # variable pour arreter le thread sur demande
+        self.enpause = False # variable pour mettre le thread en pause
+
+        self.previous_state = 1
+        self.current_state = 0
+
+    def run(self):
+        global motion
+        while self.encore:
+
+            while self.enpause:
+                time.sleep(1)
+
+            self.current_state  = GPIO.input(PIR_SENSOR)
+            #if self.previous_state == 0  and self.current_state == 1:
+            if self.current_state == 1:
+                motion = True
+                self.previous_state  = 1
+                print("motion")
+            else:
+                motion = False
+                print("no motion")
                 self.previous_state = 0
             time.sleep(3)               
 
